87dbe436727399fa6d2fdcd7aabe7d58b150cca1
Michael Shen <michael.shen.100@gmail.com>
2020-05-22 15:26:54 -0500

    Implemented simple API and textinput

---
+++ src/backend/server.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask, request, redirect, g, render_template, jsonify, session
+import requests
+
+app = Flask(__name__)
+
+# API given coordinates and distance to generate randomized route
+@app.route('/route/<lat>/<lon>/<d>')
+def route(lat, lon, d):
+    new_lat = str(int(lat) + int(d) / 69)
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + lat + "," + lon + "&destination=" + lat + "," + lon + "&mode=walking"+ "&waypoints=" + new_lat + "," + lon + "&key=AIzaSyAai4YqEuIUOKHQl8i1LgPcM6JEjyIFuuM"
+    response_dict = requests.get(url).json()
+    return response_dict
+
+if __name__ == '__main__':
+   app.run(debug = True)
--- src/screens/Trek.js
+++ src/screens/Trek.js
@@ -1,16 +1,66 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Button, View, Text, TextInput } from 'react-native';
 
 function TrekScreen({ navigation }) {
+
+  const [text, setText] = useState('');
+
   return (
+    // <View style={{padding: 10}}>
+    //   <TextInput
+    //     style={{height: 40}}
+    //     placeholder="Type here to translate!"
+    //     onChangeText={text => setText(text)}
+    //     defaultValue={text}
+    //   />
+    //   <Text style={{padding: 10, fontSize: 42}}>
+    //     {text.split(' ').map((word) => word && '🍕').join(' ')}
+    //   </Text>
+    // </View>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>What is your target distance?</Text>
-      <Button
-        title="Enter"
-        onPress={() => navigation.navigate('TrekRoute')}
+      <Text style={styles.description}>What is your target distance?</Text>
+      <Text/>
+      <TextInput
+        style={{height: 40, width: 110, fontSize: 18}}
+        textAlign='center'
+        placeholder="Eg. 5 (miles)"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+      <Text/>
+      <View style={styles.myButton}>
+        <Button
+          title="Generate Trek"
+          color="#FFFFFF"
+          onPress={() => navigation.navigate('Map')}
         />
+        </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+    padding: 20
+  },
+  title: {
+    fontSize: 60,
+    fontWeight: '500',
+    letterSpacing: 2
+  },
+  description: {
+    fontSize:22,
+    textAlign: 'center'
+  },
+  myButton:{
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    borderRadius:25,
+    backgroundColor:'#87BC5E',
+  }
+});
 
 export default TrekScreen;
